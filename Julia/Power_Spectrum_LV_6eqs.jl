5727d782d2946ecb354c8ea6e93e64424c274522
Simone Azeglio <simone.azeglio@edu.unito.it>
2020-12-08 11:02:41 -0500

    Cabrera et al. on WLC

--- Julia/Power_Spectrum_LV_6eqs.jl
+++ Julia/Power_Spectrum_LV_6eqs.jl
@@ -2,12 +2,12 @@ using DifferentialEquations
 using Plots;
 using MultivariateStats
 using SciPy
+using ProgressBars
 
-function Total_LV(du,u,p,t, r = 2.4)
+function Total_LV(du,u,p,t, r = 2.2)
   ### Total Compeition Lotka Volterra System --> WLC
   ### In this case we have a map  !!!!!
   ## http://www.math.nthu.edu.tw/~sbhsu/May-Leonard.pdf
-
   q, v, w, x, y, z = u
 
   ρ_12, ρ_13, ρ_14, ρ_15, ρ_16, ρ_21, ρ_23, ρ_24, ρ_25, ρ_26, ρ_31, ρ_32, ρ_34, ρ_35, ρ_36, ρ_41, ρ_42, ρ_43, ρ_45, ρ_46, ρ_51, ρ_52, ρ_53, ρ_54, ρ_56, ρ_61, ρ_62, ρ_63, ρ_64, ρ_65 = p
@@ -36,7 +36,7 @@ prob = DiscreteProblem(Total_LV,u0,tspan,p, saveat =dt)
 sol = solve(prob)
 u_s = Array(sol)
 #plotting
-#plot(sol, layout = (3,2))
+plot(sol, layout = (3,2))
 #savefig("solutions.png")
 
 
@@ -73,7 +73,7 @@ res_time_5 = Int.(floor.(res_time_vec(u_s[5,:])))
 res_time_6 = Int.(floor.(res_time_vec(u_s[6,:])))
 res_time = [res_time_1, res_time_2, res_time_3, res_time_4, res_time_5, res_time_6]
 
-#plot(res_time, layout = (3,2))
+plot(res_time, layout = (3,2))
 
 D_ = 6
 
@@ -110,3 +110,51 @@ end
 
 
 power_spectrum_3 = SciPy.signal.periodogram(Scheme2(res_time_3))
+plot(power_spectrum_3)
+#---------------
+r_s = 1:0.001:3
+
+spectra_3 = power_spectrum_3
+for r_i in ProgressBar(r_s)
+
+    function Total_LV(du,u,p,t, r = r_i)
+      ### Total Compeition Lotka Volterra System --> WLC
+      ### In this case we have a map  !!!!!
+      ## http://www.math.nthu.edu.tw/~sbhsu/May-Leonard.pdf
+      q, v, w, x, y, z = u
+
+      ρ_12, ρ_13, ρ_14, ρ_15, ρ_16, ρ_21, ρ_23, ρ_24, ρ_25, ρ_26, ρ_31, ρ_32, ρ_34, ρ_35, ρ_36, ρ_41, ρ_42, ρ_43, ρ_45, ρ_46, ρ_51, ρ_52, ρ_53, ρ_54, ρ_56, ρ_61, ρ_62, ρ_63, ρ_64, ρ_65 = p
+
+       du[1] = dq = r*q*(1 - q - ρ_12*v - ρ_13*w - ρ_14*x - ρ_15*y - ρ_16*z)
+       du[2] = dv = r*v*(1 - ρ_21*q -v -ρ_23*w - ρ_24*x - ρ_25*y - ρ_26*z)
+       du[3] = dw  = r*w*(1 - ρ_31*q - ρ_32*v -w - ρ_34*x - ρ_35*y - ρ_36*z)
+       du[4] = dx = r*x*(1 - ρ_41*q - ρ_42*v - ρ_43*w - x - ρ_45*y - ρ_46*z)
+       du[5] = dy = r*y*(1 - ρ_51*q - ρ_52*v - ρ_53*w - ρ_54*x - y - ρ_56*z)
+       du[6] = dz = r*z*(1 - ρ_61*q - ρ_62*v - ρ_63*w - ρ_64*x - ρ_65*y - z)
+
+    end
+
+    prob = DiscreteProblem(Total_LV,u0,tspan,p, saveat =dt)
+    #solution
+    sol = solve(prob)
+    u_s = Array(sol)
+    # Residence time
+    res_time_3 = Int.(floor.(res_time_vec(u_s[3,:])))
+    # Power Spectrum
+    power_spectrum_3 = SciPy.signal.periodogram(Scheme2(res_time_3))
+    if r_i == 1.0
+        spectra_3 = power_spectrum_3
+    else
+        spectra_3 = vcat(spectra_3, power_spectrum_3)
+    end
+
+end
+
+
+a_s = r_s[32:end]
+spectra_final = [s for s in spectra_3 if !all(isempty.(s))]
+x_s = [ a for a  in a_s]
+
+plot(x_s,first.(spectra_final), last.(spectra_final), st=:surface)
+
+size(a_s)
